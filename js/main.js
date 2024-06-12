96193a0c31d1af819811060839767519b58443d0
faisaleqbal <0221dcs285@niet.co.in>
2024-06-12 17:59:38 +0530

    Final Changes

--- js/main.js
+++ js/main.js
@@ -250,52 +250,11 @@ setTimeout(() => {
 }
 }
 
-// Class responsible for managing the theme switcher
-// class ThemeSwitcher {
-// constructor(themes, html) {
-//   this.themes = themes;
-//   this.html = html;
-//   this.init();
-// }
-
-// init() {
-//   const theme = this.getThemeFromLocalStorage();
-//   if (theme) {
-//     this.setTheme(theme);
-//   }
-
-//   this.addThemeEventListeners();
-// }
-
-// addThemeEventListeners() {
-//   this.themes.forEach((theme) => {
-//     theme.addEventListener("click", () => {
-//       const themeName = theme.getAttribute("theme");
-//       this.setTheme(themeName);
-//       this.saveThemeToLocalStorage(themeName);
-//     });
-//   });
-// }
-
-// setTheme(themeName) {
-//   this.html.setAttribute("data-theme", themeName);
-// }
-
-// saveThemeToLocalStorage(themeName) {
-//   localStorage.setItem("theme", themeName);
-// }
-
-// getThemeFromLocalStorage() {
-//   return localStorage.getItem("theme");
-// }
-// }
-
 
 
 // Instantiating the classes
 const todoItemFormatter = new TodoItemFormatter();
 const todoManager = new TodoManager(todoItemFormatter);
 const uiManager = new UIManager(todoManager, todoItemFormatter);
-const themes = document.querySelectorAll(".theme-item");
 const html = document.querySelector("html");
-const themeSwitcher = new ThemeSwitcher(themes, html);
+
